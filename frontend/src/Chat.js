ddac12acdaaa705a1431598b13efdfc6f5bb6a7e
Derth Adams <derth@me.com>
2022-02-11 14:06:07 -0800

    FNC-14 Create Chat component and styles

--- frontend/src/Chat.js
+++ frontend/src/Chat.js
@@ -1,9 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Button from "react-bootstrap/Button"
+import ButtonGroup from "react-bootstrap/ButtonGroup"
+import Col from "react-bootstrap/Col"
+import Row from 'react-bootstrap/Row'
+
+import ChatWindow from "./ChatWindow"
+import TextEntryBox from "./TextEntryBox";
+
+const sample = [
+    {
+        user: false,
+        text: "Talk to me."
+    },
+    {
+        user: true,
+        text: "I need your help. My sister has been missing for three weeks and I'm very worried about her."
+    },
+    {
+        user: false,
+        text: "Oh yeah? What's her name?"
+    },
+    {
+        user: true,
+        text: "Gladys. Gladys Nightingale."
+    },
+    {
+        user: false,
+        text: "Where did you see her last?"
+    },
+    {
+        user: true,
+        text: "At her favorite nightclub, the Mocambo. A handsome stranger asked her to dance."
+    },
+    {
+        user: false,
+        text: "And then?"
+    },
+    {
+        user: true,
+        text: "Well, I must have lost her in the crowd. I went looking for her everywhere and couldn't find her."
+    },
+    {
+        user: false,
+        text: "Did it ever occur to you that maybe she doesn't want to be found?"
+    },
+]
 
 export default function Chat() {
+    const [messages, setMessages] = useState(sample)
     return (
-            <div>
-                <h1>Chat app</h1>
+        <Col xs={9} md={6} className="mx-auto rounded-3 mb-5 chat-container">
+            <div className="message-container" tabIndex="0">
+                <Row className="mb-2 pe-3 clear-save-buttons">
+                    <Col xs={2} className="ms-auto">
+                        <ButtonGroup>
+                            <Button variant="outline-light" size="sm">
+                                <i className="bi bi-x-circle chat-button-icon"> </i>
+                            </Button>
+                            <Button variant="outline-light" size="sm">
+                                <i className="bi bi-save chat-button-icon"> </i>
+                            </Button>
+                        </ButtonGroup>
+                    </Col>
+                </Row>
+                <ChatWindow messages={messages}/>
             </div>
+            <TextEntryBox/>
+        </Col>
+
     )
 }
