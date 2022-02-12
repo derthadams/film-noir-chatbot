9998b033843f290afff291fb3368da3d04d1d67f
Derth Adams <derth@me.com>
2022-02-11 16:15:43 -0800

    FNC-11 Implement Chatbot class with method get_response

---
+++ app/chatbot/chatbot.py
@@ -0,0 +1,57 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer # noqa
+import torch # noqa
+from collections import deque
+
+# tokenizer = AutoTokenizer.from_pretrained('output-medium')
+# model = AutoModelForCausalLM.from_pretrained('output-medium')
+
+
+class Chatbot: # noqa
+    def __init__(self):
+        self.tokenizer = AutoTokenizer.from_pretrained('output-medium')
+        self.model = AutoModelForCausalLM.from_pretrained('output-medium')
+        self.queue = deque()
+        self.chat_history_ids = torch.tensor([], dtype=torch.long)
+
+    def _trim_chat_history(self):
+        if len(self.queue) >= 10:
+            discard_length = self.queue.popleft()
+            discard_length += self.queue.popleft()
+            self.chat_history_ids = self.chat_history_ids[:, discard_length]
+
+    def get_response(self, message):
+        # encode user message using tokenizer
+        user_message_input_ids = self.tokenizer.encode(message + self.tokenizer.eos_token,
+                                                       return_tensors='pt')
+
+        # add an integer representing the length of user message to end of queue
+        user_message_input_length = user_message_input_ids.size()[1]
+        self.queue.append(user_message_input_length)
+
+        # prepare input tensor for bot by concatenating chat history and user message
+        bot_input_ids = torch.cat([self.chat_history_ids, user_message_input_ids], dim=-1)
+
+        # calculate length of chat history before bot generates a response
+        prior_chat_history_length = bot_input_ids.size()[1]
+
+        # generate a bot response and add it to the end of the chat history
+        self.chat_history_ids = self.model.generate(
+            bot_input_ids, max_length=1000,
+            pad_token_id=self.tokenizer.eos_token_id,
+            no_repeat_ngram_size=3,
+            do_sample=True,
+            top_k=100,
+            top_p=0.7,
+            temperature=0.8
+        )
+
+        # calculate length of bot response and add it to the queue
+        bot_input_length = self.chat_history_ids.size()[1] - prior_chat_history_length
+        self.queue.append(bot_input_length)
+
+        # trim the head of the chat history if it's more than 10 messages long
+        self._trim_chat_history()
+
+        # return the bot response
+        return "".format(self.tokenizer.decode(self.chat_history_ids[:, bot_input_ids.shape[-1]:][0],
+                                          skip_special_tokens=True))
