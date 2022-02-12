c76edfe2f8e93490829f0b39778bd56315458fcd
Derth Adams <derth@me.com>
2022-02-11 20:36:23 -0800

    FNC-24 Scroll chat window to bottom when new messages are posted and there's an overflow

--- frontend/src/ChatWindow.js
+++ frontend/src/ChatWindow.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 import Message from "./Message"
 
 export default function ChatWindow({ messages }) {
+    const endRef = useRef(null);
+
+    const scrollToBottom = () => {
+        endRef.current?.scrollIntoView({behavior: "smooth"})
+    }
+
+    useEffect(() => {
+        scrollToBottom()
+    }, [messages])
+
     return (
         <div className="chat-window">
             {messages.map((message, index) =>
                 <Message user={message.user} text={message.text} key={index}/>
             )}
+            <div ref={endRef}/>
         </div>
     )
 }
