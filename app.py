498da9db7a5fd609fea41eec27cd967918797876
Derth Adams <derth@me.com>
2022-03-09 12:30:35 -0800

    FNC-45 Create api/load endpoint

--- app.py
+++ app.py
@@ -6,6 +6,16 @@ app = Flask(__name__)
 bot = Chatbot()
 
 
+@app.route('/api/load', methods=['POST'])
+def load():
+    if request.method == 'POST':
+        json_data = request.json
+        # bot.load_chat(json_data)
+        # print(type(json_data))
+        # print(json_data)
+        return {"status": "chat loaded"}
+
+
 @app.route('/api', methods=['POST'])
 def api():
     if request.method == 'POST':
@@ -13,7 +23,7 @@ def api():
         if json_data.get("clear"):
             bot.clear_chat_history()
             length = bot.get_chat_history_length()
-            return{"status": "chat history cleared",
+            return {"status": "chat history cleared",
                    "length": length}
         else:
             message = json_data["text"]
