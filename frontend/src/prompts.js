a22f451cd7c3e208dd2342170dd202aa1a4fba45
Derth Adams <derth@me.com>
2022-03-10 12:30:14 -0800

    FNC-30 Write a sequence of prompts promptScript with lines of dialog and delays between them

---
+++ frontend/src/prompts.js
@@ -0,0 +1,36 @@
+const promptScript = [
+    {
+        text: "Talk to me.",
+        delay: 2000
+    },
+    {
+        text: "Don't be shy.",
+        delay: 4500
+    },
+    {
+        text: "See that box down there that says \"Type a message\"?",
+        delay: 4000
+    },
+    {
+        text: "Just go ahead and type something in it.",
+        delay: 3000
+    },
+    {
+        text: "Anything.",
+        delay: 3500
+    },
+    {
+        text: "And don't forget to hit send.",
+        delay: 4000
+    },
+    {
+        text: "Cat still got your tongue?",
+        delay: 6000
+    },
+    {
+        text: "How about you just start off with \"Hello\" and see how that goes?",
+        delay: 4000
+    }
+]
+
+export default promptScript;
