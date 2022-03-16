771be4fb0061db435c7740a734c25741fcccc398
Derth Adams <derth@me.com>
2022-03-16 11:23:09 -0700

    FNC-54 Add newline to shorten line width

--- frontend/src/Message.js
+++ frontend/src/Message.js
@@ -3,9 +3,10 @@ import React from "react"
 export default function Message({ user, text }) {
     return (
             <div className={`content ${user ? "user-content" : "bot-content"}`}>
-                <div className={`message ${user ? "user-message": "bot-message"}`}>
-                    <div className={`message-text ${user ? "user-message-text" : "bot-message-text"}`}>
-                        { text }
+                <div className={`message ${user ? "user-message" : "bot-message"}`}>
+                    <div className={`message-text 
+                                    ${user ? "user-message-text" : "bot-message-text"}`}>
+                        {text}
                     </div>
                 </div>
 
