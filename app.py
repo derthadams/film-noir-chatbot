be1d620398d66676018aef4e9a1222348bb9dc78
Derth Adams <derth@me.com>
2022-03-16 11:35:24 -0700

    FNC-54 Reformat Flask app.py

--- app.py
+++ app.py
@@ -24,18 +24,14 @@ def api():
         if json_data.get("clear"):
             bot.clear_chat_history()
             length = bot.get_chat_history_length()
-            return {"status": "chat history cleared",
-                   "length": length}
+            return {"status": "chat history cleared", "length": length}
         else:
             message = json_data["text"]
             bot_response = bot.get_response(message)
-            length = bot.get_chat_history_length()
-            return {"text": bot_response,
-                    "user": False}
+            return {"text": bot_response, "user": False}
 
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def index(path):
     return render_template('index.html') # noqa
-
